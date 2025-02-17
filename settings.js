9a9544b6349f618d394a02a1cd3480f8fd56c596
sasmitha2025bot <decshopping27@gmail.com>
2025-02-17 21:44:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'king_sasmitha',
+  author: 'sasmitha_sandaken',
+  botName: "king_sasmitha",
+  botOwner: 'sasmitha sandaken', // Your name
+  ownerNumber: '94785502388', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
